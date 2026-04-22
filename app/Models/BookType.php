7e950db6460e3ee48b3e6f4b4db51119a677cca5
adityaJoestar0988 <pratamaputra0988@gmail.com>
2026-04-22 12:15:02 +0700

    edit Models Book type

--- app/Models/BookType.php
+++ app/Models/BookType.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BookType extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
