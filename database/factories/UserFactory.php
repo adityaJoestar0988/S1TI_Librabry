19b0812aaee33a96356bd8eaaec60cee12d13c32
adityaJoestar0988 <pratamaputra0988@gmail.com>
2026-04-22 11:55:44 +0700

    edit user factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => fake()->randomElement(['admin', 'user']),
         ];
     }
 
@@ -42,4 +43,18 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'admin',
+        ]);
+    }
+
+    public function user(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'user',
+        ]);
+    }    
 }
