cd8d3360727e399df41f5860026a0c7f8b8a25ef
adityaJoestar0988 <pratamaputra0988@gmail.com>
2026-05-20 11:34:15 +0700

    anjayani

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -133,7 +133,7 @@
         <div class="row g-4">
             @foreach ($books as $book )
           <!-- Buku 1 -->
-          <div class="col-md-6 col-lg-3">
+          <div class="col-md-6 col-lg-3" data-aos="zoom-out-right">
             <div class="card h-100 shadow-sm border-0">
               <img
                 src="{{ asset('storage/' . $book->image) }}"
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,6 +7,7 @@
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
       rel="stylesheet" />
+      <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
   </head>
   <body>
    @include('layouts.navigation')
@@ -17,6 +18,9 @@
     <!-- SECTION 4: FOOTER -->
     @include('layouts.footer')
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
   </body>
 </html>
